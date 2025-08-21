0a848e895c9388319898e02848d248996b3f9a6f
Daedreus <drakosoldier298@gmail.com>
2025-08-20 22:46:51 -0500

    Quartz sync: Aug 20, 2025, 10:46 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -43,8 +43,9 @@ const config: QuartzConfig = {
           // background
           light: "rgb(67, 69, 70)", 
           //text???
-          lightgray: "rgb(11, 146, 230)",
-          gray: "#rgb(64, 154, 196)",
+          lightgray: "rgb(9, 255, 0)",
+          ///rgb(64, 154, 196) backlinks?
+          gray: "#rgb(196, 64, 64)",
           darkgray: "rgb(77, 82, 85)",
           dark: "rgb(43, 44, 44)",
           secondary: "rgb(54, 136, 190)",
