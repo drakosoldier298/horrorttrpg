e13cd028441af38da2bbe99944b25b9227b6295a
Daedreus <drakosoldier298@gmail.com>
2025-08-21 11:28:17 -0500

    Quartz sync: Aug 21, 2025, 11:28 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -47,9 +47,9 @@ export const defaultContentPageLayout: PageLayout = {
         zoom: true, // whether to allow zooming in and out
         depth: 1, // how many hops of notes to display
         scale: 1.1, // default view scale
-        repelForce: 0.5, // how much nodes should repel each other
+        repelForce: 0.2, // how much nodes should repel each other
         centerForce: 0.3, // how much force to use when trying to center the nodes
-        linkDistance: 30, // how long should the links be by default?
+        linkDistance: 25, // how long should the links be by default?
         fontSize: 0.6, // what size should the node labels be?
         opacityScale: 1, // how quickly do we fade out the labels when zooming out?
         removeTags: [], // what tags to remove from the graph
@@ -61,9 +61,9 @@ export const defaultContentPageLayout: PageLayout = {
         zoom: true,
         depth: -1,
         scale: 0.9,
-        repelForce: 0.5,
+        repelForce: 0.2,
         centerForce: 0.3,
-        linkDistance: 30,
+        linkDistance: 25,
         fontSize: 0.6,
         opacityScale: 1,
         removeTags: [], // what tags to remove from the graph
